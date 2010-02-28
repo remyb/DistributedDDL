7a5291713a80e318ce8b81b3ab3374eab6ad2771
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-02-28 02:06:31 -0800

    precursor to config converter - syncing

--- part4.py
+++ part4.py
@@ -12,7 +12,6 @@ if len(sys.argv) is not 3:
 
 # load CSV
 csv_contents = loadCSV(sys.argv[2])
-#print "Loading...",csv_contents
 
 # Get Catalog and Parition Sections
 configuration = ConfigExtractor(sys.argv[1])
@@ -26,12 +25,31 @@ conn = ibm_db.pconnect(catalog['hostname'], catalog['username'],catalog['passwd'
 tablename = "BOOKST"
 nodes = get_nodes(conn, tablename)
 print "Number of Nodes: ",len(nodes)
+
 # Create a list of connected node objects
 connections = []
 for node in nodes:
   print node
-  connections.append(node)
+  connections.append(ibm_db.pconnect(node[0], node[1],node[2]))
+
+method = partition['column'].lower()
+method_types = ('notpartition', 'range', 'hash')
+if method == method_types[0]:
   
+# Partition these nodes and send to specific connection
+#column = partition['column'].lower()
+column = 'title'
+for row in csv_contents:
+  if column == 'isbn':
+    print row[0]
+  elif column == 'title':
+    print row[1]
+  elif column == 'author':
+    print row[2]
+
+# Send dtables row to catalog
+#sql = "INSERT INTO DTABLES VALUES ('%s', '%s', '%s', '%s', '%s', %s, %s, '%s', '%s', '%s');" % tablename, driver, url, user, passwd, partmtd, nodeid, partcol, partparam1, partparam2)
+#exec_query(conn, sql);
 
 
   
